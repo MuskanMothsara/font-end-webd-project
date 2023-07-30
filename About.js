6dc4c14bdb734f3219969f617acdcc1c7ba2b88e
MuskanMothsara <85925455+MuskanMothsara@users.noreply.github.com>
2023-07-31 02:06:19 +0530

    Add files via upload

---
+++ About.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function About(){
+    return(
+        <div>
+            <h1>About page</h1>
+            <p>These is about page</p>
+        </div>
+    )
+}
+export default About;
